3c2fa1dbba3941c052add56f3e9fe6f630a88b6b
kishore-sunchu <128731985+kishore-sunchu@users.noreply.github.com>
2023-07-13 20:19:37 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <div className="container">
-        hello world
-      </div>
+      <Router>
+        <Navbar />
+        <div className="container h-auto">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
     </>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <nav className="flex items-center justify-between flex-wrap p-4 bg-gray-100">
+        <div className="flex items-center flex-shrink-0 mr-6 w-1/2 lg:mr-72 text-black">
+            <div  className="w-100 h-10 mr-2">logo</div>
+        </div>
+        <div className="block lg:hidden">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400">
+            <svg
+              className={`fill-current h-3 w-3 ${isOpen ? "hidden" : "block"}`}
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+            <svg
+              className={`fill-current h-3 w-3 ${isOpen ? "block" : "hidden"}`}
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+            </svg>
+          </button>
+        </div>
+        <div
+          className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${
+            isOpen ? "block" : "hidden"
+          }`}>
+          <div className="text-base lg:flex-grow lg:flex lg:justify-evenly">
+            <Link
+              to="/"
+              className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4">
+              Home
+            </Link>
+            <Link
+              to="/"
+              className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4">
+              About Us
+            </Link>
+            <Link
+              to="/"
+              className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4">
+              Log in
+            </Link>
+            <Link
+              to="/"
+              className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4">
+              Sign in
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
